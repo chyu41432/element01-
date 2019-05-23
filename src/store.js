a20507c3ed847cc94bbd920e13c0fcedd318e3c4
chyu32 <chyu32>
2019-05-23 09:57:47 +0800

    仿去哪儿

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'store'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state:{
+
+    },
+    mutations:{
+
+    },
+    actions:{
+        
+    }
+})
